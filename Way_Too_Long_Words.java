95086d4263f184b8018ee570382e765032bb6758
KGKR <kgkr6018@gmail.com>
2025-08-23 14:04:42 +0530

    first commit

---
+++ Way_Too_Long_Words.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Way_Too_Long_Words {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for(int i=0;i<n;i++){
+           arr[i] = sc.next();
+        }
+        for(String num : arr){
+            if(num.length() > 10){
+                System.out.println(num.charAt(0) + Integer.toString(num.length() - 2) + num.charAt(num.length() - 1));
+            }
+            else{
+                System.out.println(num);
+            }
+        }
+        sc.close();
+    }
+}
